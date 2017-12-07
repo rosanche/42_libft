a78b1170fda8937dca7f7adf287d3a9d8fa1e274
Romain SANCHEZ <rosanche@e2r6p8.42.fr>
2017-12-07 19:12:05 +0100

    ajout ft_strdup.c

---
+++ ft_strdup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rosanche <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/07 19:08:13 by rosanche          #+#    #+#             */
+/*   Updated: 2017/12/07 19:11:41 by rosanche         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(char *str)
+{
+	int i;
+	char *src;
+
+	i = 0;
+	while (src[i])
+		i++;
+	if (!(src = (char *)malloc(sizeof(char) * (i + 1))))
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		src[i] = str[i];
+		i++;
+	}
+	src[i] = '\0';
+	return (src);
+}
