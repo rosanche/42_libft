4c3a43daeda65f42ea88009cb6e2cf9a46303112
Sanchez Romain <Romain@MacBook-Pro-de-Sanchez.local>
2017-12-10 21:31:24 +0100

    probleme sur itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -14,16 +14,7 @@
 
 static	char	*fill(char *tab, unsigned int nb, size_t len, int n)
 {
-<<<<<<< HEAD
-	char *tab;
-	unsigned int nb;
-	int len;
-	int i;
-	int k;
-
-=======
 	tab[len] = '\0';
->>>>>>> 7135655e3ea47981033926a62d57a7918a021271
 	nb = n;
 	if (n < 0)
 		nb = -n;
@@ -56,39 +47,8 @@ char			*ft_itoa(int n)
 		nb /= 10;
 		len++;
 	}
-<<<<<<< HEAD
-	printf("%d", len);
 	if (!(tab = (char *)malloc(sizeof(char) * (len + 1))))
 		return (NULL);
-	i = 0;
-	k = 0;
-	nb = n;
-	printf("%d", nb);
-	while (n > 0)
-	{
-		while (nb >= 10)
-		{
-			nb /= 10;
-			i++;
-		}
-		tab[k] = nb % 10 + '0';
-		k++;
-		n /= 10;
-		nb = n;
-		i = 0;
-		len--;
-	}
-	tab[k] = '\0';
+	tab = fill(tab, nb, len, n);;
 	return (tab);
-}
-
-int	main()
-{
-	printf("%s", ft_itoa(1000));
-=======
-	if (!(tab = (char *)malloc(sizeof(char) * (len + 1))))
-		return (NULL);
-	tab = fill(tab, nb, len, n);
-	return (tab);
->>>>>>> 7135655e3ea47981033926a62d57a7918a021271
 }
