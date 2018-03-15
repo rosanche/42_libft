7d440f5579abd459b36f1fe7423971ee61f7de22
Romain SANCHEZ <rosanche@e3r5p2.42.fr>
2018-03-15 16:23:18 +0100

    lst finis pas corrige

---
+++ ft_lstdelone.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rosanche <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/03/10 17:05:36 by rosanche          #+#    #+#             */
+/*   Updated: 2018/03/10 19:05:28 by rosanche         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
+{
+	if (alst)
+	{
+		if (*alst)
+			(*del)((*alst)->content, (*alst)->content_size);
+		ft_memdel((void **)alst);
+	}
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: rosanche <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/04 02:44:40 by rosanche          #+#    #+#             */
-/*   Updated: 2018/03/01 13:39:37 by rosanche         ###   ########.fr       */
+/*   Updated: 2018/03/15 16:13:56 by rosanche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,5 +77,11 @@ int		ft_toupper(int c);
 char	*ft_strdup(char *str);
 char	*ft_itoa(int n);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
+t_list	*ft_lstnew(void const *content, size_t content_size);
+void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t));
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t));
+void	ft_lstadd(t_list **alst, t_list *new_node);
+void	ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+t_list	*ft_lstmap(t_list *lst, t_list * (*f)(t_list *elem));
 
 #endif
